eab9faf278522b322a013ff5fc6957c5d4b084c8
Mac Hale <machaledesign@gmail.com>
2021-12-11 16:05:59 -0700

    first commit

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
   // API_ENDPOINT: "http://localhost:8080/api",
-  API_ENDPOINT: 'https://mac-uforia-server.herokuapp.com/api',
+  API_ENDPOINT: 'https://mac-habit-server.herokuapp.com/api',
   API_KEY: process.env.REACT_APP_API_KEY,
   TOKEN_KEY: 'uforia-client-auth-token',
 };
