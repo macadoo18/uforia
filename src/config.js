5e0a820693558100ec187f683f42ed05e0111949
Mac Hale <machaledesign@gmail.com>
2021-12-22 11:30:21 -0700

    changes api endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-  // API_ENDPOINT: "http://localhost:8080/api",
+  // API_ENDPOINT: 'http://localhost:8080/api',
   API_ENDPOINT: 'https://mac-uforia-server.herokuapp.com/api',
   API_KEY: process.env.REACT_APP_API_KEY,
   TOKEN_KEY: 'uforia-client-auth-token',
